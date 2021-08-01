67689f0155d97ecd3f9ccfab86ee78af23d98169
orelhadad <orelhadad93@gmail.com>
2021-08-01 16:12:20 +0300

    Last change

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello{
 		for(int i=1; i<=10; i++){
 			System.out.println("Hello World..." +i);
 			}
-		System.out.println("ad matai" );
+		System.out.println("Last Change" );
 		}
 	}
